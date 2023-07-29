c8076bce5b21caab4fc8a2517fca6d1ae39dea7b
MaxCapette <sk8baz@gmail.com>
2023-07-29 22:25:14 +0200

    perso html, ajout css, controller et modele pour task

---
+++ backend/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/*Route'/tasks', 'index']);
+Route::get('/tasks/{id}', 'show']);
+Route::post('/tasks', 'store']);
+Route::put('/tasks/{id}', 'update']);
+Route::delete('/tasks/{id}', 'destroy']);*/
+Route::apiResource('tasks', TaskController::class);
+// Route::apiResource('categories', CategoryController::class);
+// Route::apiResource('tags', TagController::class);
+// Route::apiResource('users', UserController::class);
