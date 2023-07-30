4b1e3aff66018066a2064ca9d8d33c5a01756367
MaxCapette <sk8baz@gmail.com>
2023-07-30 16:47:19 +0200

    category almost ok

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,6 @@ Route::post('/tasks', 'store']);
 Route::put('/tasks/{id}', 'update']);
 Route::delete('/tasks/{id}', 'destroy']);*/
 Route::apiResource('tasks', TaskController::class);
-// Route::apiResource('categories', CategoryController::class);
+Route::apiResource('categories', CategoryController::class);
 // Route::apiResource('tags', TagController::class);
 // Route::apiResource('users', UserController::class);
